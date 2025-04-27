d4eb9c3d7601ea7a015d3f10c1485900a4c5c72f
Thanh-Nguyen <nguyetha0725@gmail.com>
2025-04-27 14:33:00 -0500

    Refactor RotateWithCam to extend MonoBehaviourPun for network ownership checks

--- Team02/Assets/Scripts/Character/RotateWithCam.cs
+++ Team02/Assets/Scripts/Character/RotateWithCam.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class RotateWithCam : MonoBehaviour
+public class RotateWithCam : MonoBehaviourPun
 {
     void Update()
     {
+        if (!photonView.IsMine) return;
+
         // Rotate the object to match the camera's rotation in y direction only
         transform.rotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
     }
