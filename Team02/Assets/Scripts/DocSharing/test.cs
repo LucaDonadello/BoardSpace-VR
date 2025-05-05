ea5663d8b00617e1075d45f130c145d57b220feb
Andrew G <112661737+Djeggis@users.noreply.github.com>
2025-05-05 16:01:51 -0500

    able to upload images
    -title, can upload images onto the rawimage; working on converting pdf into image

--- Team02/Assets/Scripts/DocSharing/test.cs
+++ Team02/Assets/Scripts/DocSharing/test.cs
@@ -8,7 +8,7 @@ using UnityEngine.Networking;
 
 public class test : MonoBehaviour
 {
-    private string pdf = "application/pdf";
+    private string fileExtension = "image/*";
     [SerializeField] TMP_Text text;
     [SerializeField] RawImage image;
     private ConvertPDF convertPDF;
@@ -19,7 +19,6 @@ public class test : MonoBehaviour
             Permission.RequestUserPermission(Permission.ExternalStorageRead);
         }
         convertPDF = new ConvertPDF();
-        image = GetComponent<RawImage>();
     }
     void Update()
     {
@@ -35,19 +34,27 @@ public class test : MonoBehaviour
 					Debug.Log( "Picked file: " + path );
                     text.text = path;
 
-                    string jpegOutput = Path.Combine("./", "pdf-%d.jpg");
+                    /*string jpegOutput = Path.Combine("./", "pdf-%d.jpg");
 
-                    convertPDF.Convert(path, jpegOutput, 1, 2, "jpeg", 200, 200);
+                    convertPDF.Convert(path, jpegOutput, 1, 2, "jpeg", 200, 200);*/
 
-                    LoadPDF(jpegOutput);
+                    Texture2D texture = null;
+                    byte[] fileData;
+
+                    if(File.Exists(path)){
+                        fileData = File.ReadAllBytes(path);
+                        texture = new Texture2D(2,2);
+                        texture.LoadImage(fileData);
+                        image.texture = texture;
+                    }
                 }
-			}, new string[] { pdf } );
+			}, new string[] { fileExtension } );
         }
     }
-    IEnumerator LoadPDF(string filePath){
+    /*IEnumerator LoadPDF(string filePath){
         var www = UnityWebRequestTexture.GetTexture(filePath);
         yield return www.SendWebRequest();
         Texture2D texture = DownloadHandlerTexture.GetContent(www);
         image.texture = texture;
-    }
+    }*/
 }
