c31a8364f12fb8fd27b35d37586b0b06d7d0b9d2
Andrew G <112661737+Djeggis@users.noreply.github.com>
2025-05-05 15:32:41 -0500

    filepath grabbed, working on moving to raw image
    -tried using android java to render pdf and then grab bitmap: scrapped bc doesn't build well
    -currently working on turning pdf => jpeg => rawimage texture

--- Team02/Assets/Scripts/DocSharing/test.cs
+++ Team02/Assets/Scripts/DocSharing/test.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
 using TMPro;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class test : MonoBehaviour
 {
     private string pdf = "application/pdf";
     [SerializeField] TMP_Text text;
-    [SerializeField] PDFViewer pdfViewer;
+    [SerializeField] RawImage image;
+    private ConvertPDF convertPDF;
+    //[SerializeField] PDFViewer pdfViewer;
+    void Start() {
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead))
+        {
+            Permission.RequestUserPermission(Permission.ExternalStorageRead);
+        }
+        convertPDF = new ConvertPDF();
+        image = GetComponent<RawImage>();
+    }
     void Update()
     {
         if(Input.GetKeyDown("g") || Input.GetButtonDown("js2")){
@@ -20,9 +34,20 @@ public class test : MonoBehaviour
 				else{
 					Debug.Log( "Picked file: " + path );
                     text.text = path;
-                    pdfViewer.LoadPDF(path);
+
+                    string jpegOutput = Path.Combine("./", "pdf-%d.jpg");
+
+                    convertPDF.Convert(path, jpegOutput, 1, 2, "jpeg", 200, 200);
+
+                    LoadPDF(jpegOutput);
                 }
 			}, new string[] { pdf } );
         }
+    }
+    IEnumerator LoadPDF(string filePath){
+        var www = UnityWebRequestTexture.GetTexture(filePath);
+        yield return www.SendWebRequest();
+        Texture2D texture = DownloadHandlerTexture.GetContent(www);
+        image.texture = texture;
     }
 }
